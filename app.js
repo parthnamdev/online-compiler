08f283c3759f85dee4fc99f17166ca520f7f4b5a
parthnamdev <parthnamdev@users.noreply.github.com>
2021-03-25 16:36:46 +0530

    update app.js

--- app.js
+++ app.js
@@ -26,89 +26,18 @@ app.use('/',express.static(__dirname + '/public'));
 app.use('/', mainRouter);
 
 
-// app.get('/',(req,res)=>{
-//    res.redirect('/webpages');
-// });
-// app.get('/webpages',(req,res)=>{
-//     //console.log(str,output);
-//     var str='';
-// var output='default-output';
-//     res.render('webpages',{precode:str, outputcode:output});
-// })
-// app.get('/nextpage',(req,res)=>{
-//     res.render('nextpage') ;
-// });
-// var inp;
-// app.post('/',(req,res)=>{
-//     var str= req.body.code;
-//     inp=req.body.input;
-//     lang=req.body.selector;
-    
-//     if(lang==1)
-//     {
-//         fs.writeFile('./code.c',str,(err)=>{
-//             if(err)
-//             throw(err);
-//             var status=compc.compile();
-//             if(!status)
-//             output=compc.execute(inp);
-//             else
-//             output=status;
-//             console.log("Console output:");
-//             console.log(output);
-//             res.render('webpages',{precode:str, outputcode:output});
-//         res.end();
-//         });
-//     }
-//     else if(lang==2)
-//     {
-//         fs.writeFile('./code.cpp',str,(err)=>{
-//             if(err)
-//             throw(err);
-//             var status=compcpp.compile();
-//             if(!status)
-//             output=compcpp.execute(inp);
-//             else
-//             output=status;
-//             console.log("Console output:");
-//             console.log(output);
-//             res.render('webpages',{precode:str, outputcode:output});
-//         res.end();
-//         });
-//     }
-//     else if(lang==3)
-//     {
-//         fs.writeFile('./main.java',str,(err)=>{
-//             if(err)
-//             throw(err);
-//             var status=compjava.compile();
-//             if(!status)
-//             output=compjava.execute(inp);
-//             else
-//             output=status;
-//             //console.log("Console output:");
-//             //console.log(output);
-//             res.render('webpages',{precode:str, outputcode:output});
-//         res.end();
-//         });
-//     }
-    
-    
-//     //console.log(output);
-    
-// });
-
 const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("Server is running on PORT " + port +"...")
+});
+
 app.get('*', (req, res) => {
     res.status(404).render('404');
 });
 app.post('*', (req, res) => {
     res.status(404).render('404');
 });
-app.listen(port, () => {
-    console.log("Server is running on PORT " + port +"...")
-});
-
 // app.use((req, res) => {
 //     res.status(404).render('404');
 // });
